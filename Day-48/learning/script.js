dbe9ed83128490e135e0318d24134e6d5a8e44ef
Anurag-kawade <anurag.workzone@gmail.com>
2026-05-10 13:59:06 +0530

    Different Event Listeners

--- Day-48/learning/script.js
+++ Day-48/learning/script.js
@@ -1,18 +1,21 @@
 let box = document.querySelector("#box");
 
-// box.addEventListener("mouseenter",function(){
-//     console.log("Mouse Entered...");
-// });
+box.addEventListener("mouseenter",function(){
+    console.log("Mouse Entered...");
+});
 
-// box.addEventListener("mouseleave",function(){
-//     console.log("Mouse Left...");
-// });
+box.addEventListener("mouseleave",function(){
+    console.log("Mouse Left...");
+});
 
-// box.addEventListener("mousemove",()=>{
-//     console.log("moving...");
-// });
+box.addEventListener("mousemove",()=>{
+    console.log("moving...");
+});
 
-// box.addEventListener("mousemove",(elem)=>{
-//     console.log(elem);
-// });
+box.addEventListener("mousemove",(elem)=>{
+    console.log(elem);
+});
 
+document.body.addEventListener("keydown",(elem)=>{
+    console.log(elem.code);
+});
