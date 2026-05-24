3bfcaa80542ec4c2dabb5d65e0b34fd42cff5dc1
Anurag-kawade <anurag.workzone@gmail.com>
2026-05-24 22:23:22 +0530

    Advance JS

---
+++ Day-56/script.js
@@ -0,0 +1,14 @@
+// OOPS -> Object Oriented Programming System
+
+// jab code chota hota hai toh fir aap kuch bhi kr sakte ho bina koi guideline follow kiye and jab code bada ho jata hai tab role mai aata hai guidelines 
+
+// now we will write the code in the form of objects , classes and functions 
+
+// creating an object 
+
+let user1 = {
+    name : "Anurag Suresh Kawade",
+    age : 22,
+    email : "anurag@gmail.com"
+};
+
