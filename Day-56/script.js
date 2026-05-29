8fe22917d01983fd34f9a3a65924d6a18fc75c0c
Anurag-kawade <anurag.workzone@gmail.com>
2026-05-29 18:25:52 +0530

    Advance JS - OOPS Concepts(Classes and Constructors Questions)

--- Day-56/script.js
+++ Day-56/script.js
@@ -4,6 +4,7 @@
 
 // now we will write the code in the form of objects , classes and functions 
 
+// ek bande ki details ek sath rakhne ke liye aapke pas objects hai 
 // creating an object 
 
 let user1 = {
@@ -11,4 +12,23 @@ let user1 = {
     age : 22,
     email : "anurag@gmail.com"
 };
+
+// classes
+// jabhi class banate ho , toh class ka nam Capital Letter se shuru hona jaruri hai 
+// hamesha pehli chiz , constructor banao 
+// classes banate hai taki hame har bar ek naya object mil jaye 
+// classes object ki factory hai 
+// har bar new keyword ke sath class run karoge to ek naya object milega    
+
+class BiscuitMaker{
+    constructor(brandName,price,color){
+        this.name = brandName;
+        this.price = price;
+        this.color = color;
+    }
+}
+
+let biscuit1 = new BiscuitMaker("Parle-G",5,"Brown");
+let biscuit2 = new BiscuitMaker("Marie-Gold",10,"Reddish-Brown");
+let biscuit3 = new BiscuitMaker("Krack-Jack",20,"Brownish");
 
