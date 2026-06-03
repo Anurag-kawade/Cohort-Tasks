2260a0a2f5c1bec1c55b27595a33522e91629e6b
Anurag-kawade <anurag.workzone@gmail.com>
2026-06-03 22:44:41 +0530

    Prototype and Constructor Concepts in JS OOPS

---
+++ Day-57/script.js
@@ -0,0 +1,17 @@
+class MineralWater{
+    constructor(name,color,price){
+        this.name = name;
+        this.color = color;
+        this.price = price;
+    }
+}
+
+let mineralWater1 = new MineralWater("Bisleri","Blue",20);
+let mineralWater2 = new MineralWater("Kinley","White-Blue",18);
+let mineralWater3 = new MineralWater("Amrut","Black",15);
+let mineralWater4 = new MineralWater("Rajhans","Bluish",10);
+
+
+
+
+// Prototypes -> shared memory 
