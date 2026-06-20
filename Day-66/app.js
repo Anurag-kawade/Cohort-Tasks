18d0fc56ad79d725ac19aac1c76a2875320176f5
Anurag-kawade <anurag.workzone@gmail.com>
2026-06-20 20:18:37 +0530

    Prerequisite for React

---
+++ Day-66/app.js
@@ -0,0 +1,5 @@
+function add(a,b){
+    return a + b;
+}
+
+export default add;   // ek hi bar export default kr sakte hai ; i.e a module can not have multiple default exports 
---
+++ Day-66/script.js
@@ -0,0 +1,4 @@
+// import add from "./app.js"
+import addition from "./app.js"   // ye bhi chaelga,i.e same nam se nahi import kiya fir bhi chalega , the reason is kyu ki tum default export kr rahe ho isliye kuch bhi nam chalega , default export mai mattter nahi krta 
+
+console.log(addition(5,2));
