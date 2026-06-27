687b48b46ac3aa2e86f1da63163806326f160356
Anurag-kawade <anurag.workzone@gmail.com>
2026-06-27 20:55:25 +0530

    the concept of props

---
+++ Day-71/src/App.jsx
@@ -0,0 +1,15 @@
+import Card from "./components/Card.jsx"
+
+const App = () => {
+  return (
+    <div>
+      <>
+      {Card(10,20)}
+      {Card(30,20)}
+      </>
+      </div>
+
+  )
+}
+
+export default App
---
+++ Day-71/src/components/Card.jsx
@@ -0,0 +1,7 @@
+const Card = (a,b) => {
+  return (
+    <div>This is Card {a+b}</div>
+  )
+}
+
+export default Card
