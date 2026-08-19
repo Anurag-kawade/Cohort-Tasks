a3eb2cf34bc1cc80f9a3cae4006f1a64f6f492df
Anurag-kawade <anurag.workzone@gmail.com>
2026-08-19 22:38:51 +0530

    Rest APIs

---
+++ Day-88/src/app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+
+const app = express()
+
+app.use(express.json())
+
+const notes = []
+
+app.get('/',(req,res)=>{
+    res.send('Hello World...')
+})
+
+app.post('/notes',(req,res)=>{
+    console.log(req.body)
+    res.send('notes created')
+    notes.push(req.body)
+    console.log(notes)
+})
+
+app.get('/notes',(req,res)=>{
+    res.send(notes)
+})
+
+app.delete('/notes/:index',(req,res)=>{
+    delete notes[req.params.index]
+    res.send('notes deleted successfully')
+})
+
+app.patch('/notes/:index',(req,res)=>{
+    notes[req.params.index].description = req.body.description
+    res.send('notes updated succesfully')
+})
+
+module.exports = app
